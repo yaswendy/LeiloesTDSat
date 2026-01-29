8607025223a88625e7b6d68290ba97d62ec41c38
Yasminie <yasminiewendyl.costa@gmail.com>
2026-01-29 10:27:25 -0400

    Funcionalidade salvar cadastrando produtos com sucesso

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -23,13 +23,19 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        try {
+            prep = conn.prepareStatement(sql); 
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());   
+            prep.setString(3, produto.getStatus());
+            prep.execute();
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");      
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + erro.getMessage());
+        }
     }
-    
     public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -150,7 +150,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
         
         ProdutosDAO produtodao = new ProdutosDAO();
         produtodao.cadastrarProduto(produto);
-        
+     
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
